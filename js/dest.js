65337fa774dc3cdd6e6eb6efcacf54f5e5a6c316
Isao Yagi <isao@yahoo-inc.com>
2012-06-29 04:02:10 -0400

    minor, not work?

--- js/dest.js
+++ js/dest.js
@@ -9,6 +9,7 @@ function getThumbs(pics) {
     YUI().use('yql', function (Y) {
         var qry = 'select source from flickr.photos.sizes where photo_id in (select id from flickr.photos.search(1) where woe_id in (select woeid from geo.places(1) where text in ("Acapulco","Accra","Amsterdam","Antigua","Athens","Atlanta","Bangkok","Barcelona","Beijing","Belize City","Bermuda","Berlin","Boston","Brussels","Budapest","Buenos Aires","Busan","Cabo San Lucas","Cancun","Copenhagen","Dubai","Dusseldorf","Frankfurt","Freeport","Guam","Guayaquil","Hong Kong","Honolulu","Istanbul","Johannesburg","Kingston","Lagos","London","Las Vegas","Los Angeles","Madrid","Malaga","Managua","Manchester","Manila","Miami","Milan","Minneapolis","Mumbai","Nagoya","Nassau Bahamas","New York","Nice","Osaka","Koror Palau","Paris","Pisa","Port of Spain","Portland","Prague","Quito","Rio de Janeiro","Rome","Saipan","Salt Lake City","San Diego","San Francisco","San Jose","San Juan","San Pedro Sula","San Salvador","Santiago","Santo Domingo","Sao Paulo","Seattle","Seoul","Shanghai","Shannon","Singapore Singapore","Stockholm","Sydney","Taipei","Tel Aviv","Tokyo","Grand Turk","Venice","Washington DC","Zurich")) and sort="interestingness-desc" and min_taken_date = "2000-01-01 00:00:00" and api_key="5b7b21a3d44ed659e13410edee783a3b") and api_key="5b7b21a3d44ed659e13410edee783a3b" and label="Large"';
         if(pics) {
+            // for testing cause real pics take a while to load
             renderThumbs(pics);
             scrollView();
             thumbClicks();
@@ -50,25 +51,30 @@ function thumbClicks() {
     YUI().use('node-event-delegate', function (Y) {
 
         Y.one('#thumbs').delegate('click', function() {
-            var src = this.get('src'),
-                file = encodeURI(src);//.replace(/^.*[\\\/]/, '');
-            
-            Y.one('a.next').set('href', 'hotel.html?file=' + file);
+            var src = this.get('src');
+            Y.one('a.next').set('href', 'hotel.html?src=' + encodeURI(src));
             Y.one('div.foo').set('innerHTML', '<img src=' + src + ' width="670" height="455">');
-            
         }, 'img');
 
     });
 }
 
 function fetchLocation(file) {
-	var qry = 'select * from flickr.photos.info where photo_id=@filename and api_key="5b7b21a3d44ed659e13410edee783a3b"';
+    YUI().use('yql', function (Y) {
+        var qry = 'select * from flickr.photos.info where photo_id=@file and api_key=@apikey';
+        Y.YQL(qry, function(r) {
+            console.log(r);
+            var town = r.query.results.photo.location.locality.content,
+                country = r.query.results.photo.location.country.content;
 
-    Y.YQL(qry, function(r) {
-        town = r.query.results.photo.location.locality.content;
-        country = r.query.results.photo.location.country.content;
-        fetchHotels(town,country);
-    }, param);
+            fetchHotels(town, country);
+
+        }, {
+            file: file.split('_')[0],
+            apikey: '5b7b21a3d44ed659e13410edee783a3b',
+            debug:1
+        });
+    });
 }
 
 function fetchHotels(town, country) {
