a34fd23cdc99c406cf56b9c5a469d44efea716d9
Isao Yagi <isao@yahoo-inc.com>
2012-06-28 23:58:57 -0400

    server

---
+++ index.js
@@ -0,0 +1,10 @@
+var static = require('node-static'),
+    file = new static.Server();
+
+require('http').createServer(function (request, response) {
+
+    request.addListener('end', function () {
+        file.serve(request, response);
+    });
+
+}).listen(80);
